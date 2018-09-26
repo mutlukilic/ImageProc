e9c0006b7a12086f7c00e484f61b6af679fd210d
Mutlu Kılıç <mutlukilic1919@gmail.com>
2018-09-26 19:46:05 +0300

    Update ders1.py

--- ders1.py
+++ ders1.py
@@ -1,44 +1,33 @@
 liste=[0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,0,1,2,0,1]
-
 for i in liste:
     print(int(i)+1)
 
 for i in range(0,len(liste)):
     liste[i]=liste[i]+1
-
 print(liste)
-
 import random
-
 s=random.randint(2,5)
 print(s)
-
 listee=[]
 for i in range(10):
     listee.append(random.randint(0,10))
 print(listee)
-
 test_sayısı=100000
-
 def createArray(s):
     myList=[]
     for i in range(s):
         myList.append(random.randint(0,10))
     return myList
-
 def printArray(array):
     print(array)
-    
 def incArray(array):
     myList_1=[]
     for i in array:
         myList_1.append(i+1)
     return myList_1
-
 def createArrayVersion(s):
     myList=np.arange(s)
     return myList
-
 dizi_1=createArray(test_sayısı)
 printArray(dizi_1)
 dizi_2=incArray(dizi_1)
@@ -47,7 +36,6 @@ printArray(dizi_2)
 import numpy as np
 x=np.arange(10)
 x
-
 myL=createArrayVersion(test_sayısı)
 myL=myL+25
 
